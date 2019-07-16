dbacc651d6f55c3988dadc35c83f71b60eb74832
Andrey <stepgt@mail.ru>
2019-07-16 11:36:32 +1000

    Init project

---
+++ src/Components/ToDoList/ToDoList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './ToDoList.scss';
+
+class ToDoList extends Component {
+  render() {
+    return (
+        <div className="react_todolist">
+          <div>
+            ToDoList
+          </div>
+        </div>
+    );
+  };
+}
+
+export default ToDoList;
