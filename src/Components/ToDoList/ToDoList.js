490b67af3d6c467a63554853b04099ab6b85f3aa
Andrey <stepgt@mail.ru>
2019-07-18 11:36:21 +1000

    Added markup and state. Added some styles

--- src/Components/ToDoList/ToDoList.js
+++ src/Components/ToDoList/ToDoList.js
@@ -2,11 +2,25 @@ import React, {Component} from 'react';
 import './ToDoList.scss';
 
 class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: ['Learn JS', 'Learn ReactJS']
+    };
+  }
+
   render() {
     return (
         <div className="react_todolist">
-          <div>
-            ToDoList
+          <div className="react_todolist__header">
+            <input type="text"/>
+          </div>
+          <div className="react_todolist__tasks">
+            {this.state.tasks.map((el, key) => {
+              return <div key={key} className="react_todolist__task">
+                {el}
+              </div>
+            })}
           </div>
         </div>
     );
