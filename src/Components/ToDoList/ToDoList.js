778a87852553ddcad03bdaa5466bad6e4e1b8e23
Andrey <stepgt@mail.ru>
2019-07-18 11:50:04 +1000

    Added new tasks in state

--- src/Components/ToDoList/ToDoList.js
+++ src/Components/ToDoList/ToDoList.js
@@ -9,11 +9,22 @@ class ToDoList extends Component {
     };
   }
 
+  createNewTask(e) {
+    if (e.key === 'Enter') {
+      const newTasksList = this.state.tasks;
+      newTasksList.push(e.currentTarget.value);
+      e.currentTarget.value = '';
+      this.setState({
+        tasks: newTasksList
+      });
+    }
+  }
+
   render() {
     return (
         <div className="react_todolist">
           <div className="react_todolist__header">
-            <input type="text"/>
+            <input onKeyPress={this.createNewTask.bind(this)} type="text"/>
           </div>
           <div className="react_todolist__tasks">
             {this.state.tasks.map((el, key) => {
