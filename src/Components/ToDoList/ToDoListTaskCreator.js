beb5060e8e929e5a700c88312bcf0b23811a317b
Andrey <stepgt@mail.ru>
2019-08-05 10:47:49 +1000

    Refactor task creator to functionality

--- src/Components/ToDoList/ToDoListTaskCreator.js
+++ src/Components/ToDoList/ToDoListTaskCreator.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import {ToDoListServicesCreateTask} from './ToDoListServices';
 
-class ToDoListTaskCreator extends Component {
-  createNewTask(e) {
+const ToDoListTaskCreator = (props) => {
+  const createNewTask = (e) => {
     if (e.key === 'Enter') {
       const newTaskInput = e.currentTarget;
       ToDoListServicesCreateTask(newTaskInput.value, 53236)
@@ -12,19 +12,15 @@ class ToDoListTaskCreator extends Component {
               isDone: data.task.done,
               id: data.task.id
             };
-            this.props.parentCreateNewTask(newTask);
+            props.parentCreateNewTask(newTask);
             newTaskInput.value = '';
           });
     }
-  }
-
-  render() {
-    return (
-        <div className="react_todolist__header">
-          <input onKeyPress={this.createNewTask.bind(this)} type="text"/>
-        </div>
-    );
   };
-}
-
+  return (
+      <div className="react_todolist__header">
+        <input onKeyPress={createNewTask} type="text"/>
+      </div>
+  );
+};
 export default ToDoListTaskCreator;
