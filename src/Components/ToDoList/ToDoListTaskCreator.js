8b9167fd88e177c0f9aa3fa26c4fe50ea4d6c478
Andrey <stepgt@mail.ru>
2019-08-05 12:35:18 +1000

    Rename task creator to form presentation and added keyup handler value

--- src/Components/ToDoList/ToDoListTaskCreator.js
+++ src/Components/ToDoList/ToDoListTaskCreator.js
@@ -2,7 +2,25 @@ import React, {Component} from 'react';
 import {ToDoListServicesCreateTask} from './ToDoListServices';
 
 class ToDoListFormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      isWaiting: false
+    };
+  }
+
+  changeTitle(title) {
+    this.setState({
+      title: title
+    });
+  }
+
   createTask(title) {
+    this.setState({
+      isWaiting: true,
+      title: title
+    });
     ToDoListServicesCreateTask(title, 53236)
         .then(data => {
           const newTask = {
@@ -11,27 +29,41 @@ class ToDoListFormContainer extends Component {
             id: data.task.id
           };
           this.props.parentCreateNewTask(newTask);
-          //newTaskInput.value = '';
+          this.setState({
+            isWaiting: false,
+            title: ''
+          });
         });
   }
 
   render() {
+    let {title, isWaiting} = this.state;
     return (
-        <ToDoListTaskCreator createTask={this.createTask.bind(this)}/>
+        <ToDoListFormPresentation changeTitle={this.changeTitle.bind(this)}
+                                  createTask={this.createTask.bind(this)}
+                                  title={title} isWaiting={isWaiting}/>
     );
   }
 }
 
-const ToDoListTaskCreator = (props) => {
+const ToDoListFormPresentation = (props) => {
   const createNewTask = (e) => {
     if (e.key === 'Enter') {
       const newTaskInput = e.currentTarget;
       props.createTask(newTaskInput.value);
     }
   };
+
+  const changeTitle = (e) => {
+    props.changeTitle(e.currentTarget.value)
+  };
+
   return (
-      <div className="react_todolist__header">
-        <input onKeyPress={createNewTask} type="text"/>
+      <div className="header">
+        <input onKeyUp={changeTitle}
+               onKeyPress={createNewTask} /*value={props.title}*/
+               disabled={props.isWaiting}
+               onChange={() => {}}/>
       </div>
   );
 };
